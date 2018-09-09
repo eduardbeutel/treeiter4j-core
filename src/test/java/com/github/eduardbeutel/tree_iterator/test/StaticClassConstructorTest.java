40d7d8ef588cd6eff141ed55b9ff8241e4a01396
Eduard Beutel <eduard.beutel@gmx.de>
2018-09-09 12:36:43 +0200

    Added test that calls the constructor of static classes because jacoco counts it towards code coverage.

---
+++ src/test/java/com/github/eduardbeutel/tree_iterator/test/StaticClassConstructorTest.java
@@ -0,0 +1,27 @@
+package com.github.eduardbeutel.tree_iterator.test;
+
+import com.github.eduardbeutel.tree_iterator.core.OperationCreator;
+import com.github.eduardbeutel.tree_iterator.core.PredicateCreator;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StaticClassConstructorTest
+{
+
+    List<Class> staticClasses = Arrays.asList(
+            OperationCreator.class,
+            PredicateCreator.class
+    );
+
+    @Test
+    public void callConstructor() throws IllegalAccessException, InstantiationException
+    {
+        for(Class clazz: staticClasses)
+        {
+            clazz.newInstance();
+        }
+    }
+
+}
